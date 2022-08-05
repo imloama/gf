40e521b0a212115d8b3b7b61b16b022693596bfc
imloama <imloama@gmail.com>
2022-08-05 15:09:58 +0800

    feat: 去掉导致内存占用暴增的问题

--- os/gcache/gcache_cache.go
+++ os/gcache/gcache_cache.go
@@ -8,9 +8,7 @@ package gcache
 
 import (
 	"context"
-	"time"
 
-	"github.com/gogf/gf/v2/os/gtimer"
 	"github.com/gogf/gf/v2/util/gconv"
 )
 
@@ -31,7 +29,7 @@ func New(lruCap ...int) *Cache {
 	}
 	// Here may be a "timer leak" if adapter is manually changed from memory adapter.
 	// Do not worry about this, as adapter is less changed, and it does nothing if it's not used.
-	gtimer.AddSingleton(context.Background(), time.Second, memAdapter.(*AdapterMemory).syncEventAndClearExpired)
+	// gtimer.AddSingleton(context.Background(), time.Second, memAdapter.(*AdapterMemory).syncEventAndClearExpired)
 	return c
 }
 
